eb81d6624eb71c7efdaecbbc9002aa21613db58e
Jonatan Medina <136943363+jonatanmedina12@users.noreply.github.com>
2024-12-11 20:37:34 -0500

    V2
    V2

--- config-server/src/main/java/com/cliente/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/cliente/config_server/ConfigServerApplication.java
@@ -1,15 +1,17 @@
-package com.MicroServicios.companies_crud;
+package com.cliente.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
-public class CompaniesCrudApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CompaniesCrudApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
